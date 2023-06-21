ede8a8e64409a0765f9c9abb16d3bd656f356d62
Nerro <nduguiskarioti@gmail.com>
2023-06-21 17:51:21 +0300

    Installed packages

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express")
 const bcrypt = require("bcrypt")
 const app = express()
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+app.use(express.urlencoded({extended: true}))
+
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req,res)=>{
+    res.render("home")
+})
 
 app.listen(PORT, ()=>{
     console.log("LISTENING ON PORT 3000")

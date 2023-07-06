ba65c5fdd6811cc4b583797266b05ac51143c6bb
Nerro <nduguiskarioti@gmail.com>
2023-07-06 17:54:41 +0300

    forms

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const express = require("express")
-const bcrypt = require("bcrypt")
-const app = express()
-app.set("view engine", "ejs")
-app.use(express.static("public"))
-app.use(express.urlencoded({extended: true}))
+const express = require("express");
+const bcrypt = require("bcrypt");
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req,res)=>{
-    res.render("home")
-})
-
-app.listen(PORT, ()=>{
-    console.log("LISTENING ON PORT 3000")
-}) 
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/forms", (req, res) => {
+  res.render("forms");
+});
+app.listen(PORT, () => {
+  console.log("LISTENING ON PORT 3000");
+});

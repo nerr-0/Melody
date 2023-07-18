244477f5e2ef06022a5e2604de4c3c171bf7152b
Nerro <nduguiskarioti@gmail.com>
2023-07-18 16:16:45 +0300

    Server side start

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const mysql = require("mysql")
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "test"
+})
+con.connect((error)=>{
+  if(error){
+    res.render("error")
+  }else{
+    console.log("CONNECTED")
+  }
+})
 
 const PORT = process.env.PORT || 3000;
 
